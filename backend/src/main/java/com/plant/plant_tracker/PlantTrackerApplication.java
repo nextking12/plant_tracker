0ac57aefaf9c9a6814d2716feace2fb708d0f1d3
Edward King <king8three@gmailcom>
2024-11-22 21:10:47 -0500

    work on backend

---
+++ backend/src/main/java/com/plant/plant_tracker/PlantTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.plant.plant_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/plant/plant_tracker/PlantTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plant.plant_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
